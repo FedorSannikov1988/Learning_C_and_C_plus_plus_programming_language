a94a541e57d7bb9432629706782620b11f7654b4
Fedor <fedor.sannikov88@gmail.com>
2025-10-24 08:52:20 +0700

    add exercise1 to function_as_result_another_function

---
+++ C_programming_language/exercises_with_metanit/functions/function_as_result_another_function/exercise1.c
@@ -0,0 +1,44 @@
+/*
+Exercise 1:
+
+Упражнение 1:
+*/
+#include <stdio.h>
+  
+typedef int (binary_op)(int, int);
+ 
+int sum(int x, int y) {return x + y;}
+int subtract(int x, int y) {return x - y;}
+int multiply(int x, int y) {return x * y;}
+int operation(binary_op op, int a, int b) {return op(a, b);}
+
+// void (*function)(char *, int); <- указатель на функцию
+// тип (*имя_указателя) (типы_параметров);
+
+// int (*select(int choice))(int, int) <- функция select возврацает указатель на функцию
+
+//  int choice - выбранный пункт
+binary_op * select(int choice)
+{
+    // возвращаем нужную функцию
+    switch (choice)
+    {
+        case 2:
+            return &subtract;
+        case 3:
+            return &multiply;
+        default:
+            return &sum;
+    }
+}
+
+int main(void)
+{
+    binary_op* operation = select(1);  // получаем указатель на функцию sum - сложение
+    int result = operation(6, 4); // выполняем функцию
+    printf("result: %d \n", result); // result: 10
+ 
+    return 0;
+}
+
+//gcc exercise1.c -o exercise1 && ./exercise1
